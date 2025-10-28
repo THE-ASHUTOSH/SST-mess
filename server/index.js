21a30239ff56c8d88de665d582f9ac850d033819
THE-ASHUTOSH <ashubham1906@gmail.com>
2025-10-28 23:47:23 +0530

    optimized

--- server/index.js
+++ server/index.js
@@ -6,25 +6,29 @@ import "./config/passport.js";
 import vendorModel from "./models/vendor.model.js";
 import connectDB from "./config/dbconfig.js";
 import vendorRoute from "./routes/vendor.route.js";
-import cookieParser from "cookie-parser";   
+import cookieParser from "cookie-parser";
 
-dotenv.config({quiet: true});
+dotenv.config({ quiet: true });
 
-const app  = express();
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
-app.use(cors(
-    {
-       origin: ["http://localhost:3000", "http://127.0.0.1:3000",process.env.CLIENT_URL],
-        credentials:true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-    }
-));
-app.use("/auth",authRouter);
-app.use("/vendor",vendorRoute);
-connectDB()
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            process.env.CLIENT_URL,
+        ],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+app.use("/auth", authRouter);
+app.use("/vendor", vendorRoute);
+connectDB();
 
 // app.get("/addvendor",async(req,res)=>{
 //     try {
@@ -42,6 +46,6 @@ connectDB()
 //     }
 // });
 
-app.listen(process.env.PORT || 5000,"0.0.0.0",()=>{
-    console.log("Server is running on port 5000");
-})
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+    console.log("Server is running on port ", process.env.PORT || 5000);
+});

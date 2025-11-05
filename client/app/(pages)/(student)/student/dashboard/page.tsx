0581355fb169dde752ed4085aa31c8ca2d099034
Ashutosh Kumar <82095076+THE-ASHUTOSH@users.noreply.github.com>
2025-11-06 02:14:34 +0530

    Update page.tsx

--- client/app/(pages)/(student)/student/dashboard/page.tsx
+++ client/app/(pages)/(student)/student/dashboard/page.tsx
@@ -6,7 +6,7 @@ const dashboard = () => {
     <div className="p-4 sm:p-6 lg:p-8">
       <h1 className="text-4xl font-extrabold text-center mb-10 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-500 to-indigo-400 tracking-tight transform hover:scale-105 transition-transform duration-300">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 items-stretch">
-
+        {console.log("work")}
         <EventCard title="Menu" redirectUrl="/student/vendorDetails">
           <p>Get the menu of all the vendors here</p>
         </EventCard>

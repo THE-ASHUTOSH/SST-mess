aff002fc1cdb337a2db303349345c7beb4089807
THE-ASHUTOSH <ashubham1906@gmail.com>
2025-12-07 12:06:31 +0530

    rate limit added

--- server/middlewares/ratelimiter.middleware.js
+++ server/middlewares/ratelimiter.middleware.js
@@ -12,7 +12,7 @@ const apiLimiter = rateLimit({
 // Stricter rate limiter for authentication routes
 const authLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 10, // Limit each IP to 10 requests per window
+  max: 25, // Limit each IP to 10 requests per window
   standardHeaders: true,
   legacyHeaders: false,
   message: 'Too many authentication attempts from this IP, please try again after a minute',

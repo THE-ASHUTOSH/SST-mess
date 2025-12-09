c9c7f43a44dda217d8cd7c08607fbfb6259fe385
THE-ASHUTOSH <ashubham1906@gmail.com>
2025-12-09 11:26:52 +0530

    security check

--- client/lib/axiosInstance.ts
+++ client/lib/axiosInstance.ts
@@ -3,6 +3,9 @@ import { getToken } from './auth';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 axiosInstance.interceptors.request.use(
